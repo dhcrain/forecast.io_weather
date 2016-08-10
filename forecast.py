d2bdfe378bf4baed687823c48672ddb8c514c629
dhcrain <dhcrain@gmail.com>
2016-08-09 22:17:20 -0400

    Rearanged data into a table, but if there is a long summary it messes up the formating.

--- forecast.py
+++ forecast.py
@@ -2,32 +2,36 @@ import datetime
 import os
 import requests
 import geocoder
+from tabulate import tabulate
+
+forecast_api_key = os.environ['forecast_api']
 
 print("Enter a location, Zip Code (28768) * OR * City, State (Greenville, SC)")
 location = input("Location? ")
 g = geocoder.google(location)
 
-api_key = os.environ['forecast_api']
-
-lat = g.latlng[0]
-lng = g.latlng[1]
-
-url = "https://api.forecast.io/forecast/{}/{},{}".format(api_key, lat, lng)
+url = "https://api.forecast.io/forecast/{}/{},{}".format(forecast_api_key, g.latlng[0], g.latlng[1])
 response = requests.get(url).json()
-
+table_week = []
 print("\n" + response['daily']['summary'])
 for day in response['daily']['data']:
-    time = datetime.datetime.fromtimestamp(int(day['time'])).strftime('%A %B %-d')
+    time = datetime.datetime.fromtimestamp(int(day['time'])).strftime('%a %b %-d')
     summary = day['summary']
-    max_temp = int(day['temperatureMax'])
-    max_temp_time = datetime.datetime.fromtimestamp(int(day['temperatureMaxTime'])).strftime('%-I%p')
-    min_temp = int(day['temperatureMin'])
-    min_temp_time = datetime.datetime.fromtimestamp(int(day['temperatureMinTime'])).strftime('%-I%p')
-    percip_chance = int(day['precipProbability'] * 100)
+    max_temp = str(int(day['temperatureMax'])) + "°F"
+    max_temp_time = "at " + str(datetime.datetime.fromtimestamp(int(day['temperatureMaxTime'])).strftime('%-I%p'))
+    min_temp = str(int(day['temperatureMin'])) + "°F"
+    min_temp_time ="at " + str(datetime.datetime.fromtimestamp(int(day['temperatureMinTime'])).strftime('%-I%p'))
+    percip_chance = str(int(day['precipProbability'] * 100)) + "%"
     try:
         percip_type = "of " + day['precipType']
     except KeyError:
-        percip_type = ""
-    print("""
-{} - {}
-* h: {}F at {} * l: {}F at {} * {}% {}""".format(time, summary, max_temp, max_temp_time, min_temp, min_temp_time, percip_chance, percip_type))
+        percip_type = " "
+#     print("""
+# {} - {}
+# * h: {}°F at {} * l: {}°F at {} * {}% {}""".format(time, summary, max_temp, max_temp_time, min_temp, min_temp_time, percip_chance, percip_type))
+    table_day = [time, summary, max_temp, max_temp_time, min_temp, min_temp_time, percip_chance, percip_type]
+    table_week.append(table_day)
+
+
+headers = ["", "", "hi", "", "lo", "", "percip", ""]
+print(tabulate(table_week, headers=(headers), tablefmt="psql"))
