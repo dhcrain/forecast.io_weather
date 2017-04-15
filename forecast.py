b72cc540b933beffa3c6840a9f9ecc7d3b70e6cc
Davis Crain <dhcrain@gmail.com>
2017-04-14 20:39:14 -0400

    Can use lists not tuples, yeah

--- forecast.py
+++ forecast.py
@@ -49,7 +49,7 @@ def get_weather(location):
 
     url = "https://api.darksky.net/forecast/{}/{},{}?exclude=minutely,hourly,alerts,flags?units={}".format(darksky_api_key, g.latlng[0], g.latlng[1], units)
     response = requests.get(url).json()
-    table_week = (("", "", "HI", "at", "LO", "at", "", ""),)
+    table_week = [["", "", "HI", "at", "LO", "at", "", ""]]
     for day in response['daily']['data']:
         date = datetime.datetime.fromtimestamp(int(day['time'])).strftime('%a %b %-d')
         summary = day['summary']
@@ -62,11 +62,10 @@ def get_weather(location):
             percip_type = day['precipType'].title()
         except KeyError:
             percip_type = ""
-        table_day = (date, summary, max_temp, max_temp_time, min_temp, min_temp_time, percip_chance, percip_type)
-        table_week += (table_day,)
+        table_day = [date, summary, max_temp, max_temp_time, min_temp, min_temp_time, percip_chance, percip_type]
+        table_week.append(table_day)
 
     table_week[1][0] = colors.GREEN + "* Today *" + colors.ENDC
-    
     print(colors.UNDERLINE + "\nForecast for" + colors.ENDC + ": {}, {}".format(g.city, g.state))
     print(colors.UNDERLINE + "Current Temp" + colors.ENDC + ": " + str(int(response['currently']['temperature'])) + unit_letter)
     print("\n" + colors.BOLD + response['daily']['summary'] + colors.ENDC)
